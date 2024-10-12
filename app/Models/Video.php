8b0f0795cad947abe1dccf606ba9481e7dacc829
Hichem <hichem.dimassi@liberrex.com>
2024-10-12 16:58:58 +0100

    added model views for videos

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -4,12 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Auth;
 
 class Video extends Model
 {
     use HasFactory;
 
-    // public function channel(): BelongsTo {
-    //     return $this->belongsTo(Channel::class);
-    // }
+    public function channel(): BelongsTo {
+        return $this->belongsTo(Channel::class);
+    }
+
+    public function views(): HasMany {
+        return $this->hasMany(View::class);
+    }
+
+    // Method to get the count of views
+    public function getViewsCount()
+    {
+        return $this->views()->count();
+    }
+
+    public function editable()
+    {
+        return Auth::check() && Auth::id() == $this->channel->user_id;
+    }
 }
---
+++ app/Models/View.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class View extends Model
+{
+    use HasFactory;
+
+    public function Video(): BelongsTo {
+        return $this->belongsTo(Video::class);
+    }
+
+    public function User(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+}
